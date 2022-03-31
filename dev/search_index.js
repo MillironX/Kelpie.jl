8435b1c710b41c858fe9a11afd4a94b2d958a11d
Documenter.jl <documenter@juliadocs.github.io>
2022-03-31 23:31:20 +0000

    build based on d9f751e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Kelpie","category":"page"},{"location":"#Kelpie","page":"Home","title":"Kelpie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Kelpie.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Kelpie]","category":"page"}]
+}
