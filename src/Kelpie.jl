8a86c309de796cccc205cc670eddc3b926e1bade
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-04-05 15:38:34 -0500

    Add doctest for html function
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- src/Kelpie.jl
+++ src/Kelpie.jl
@@ -131,6 +131,17 @@ end #function
     html(content...=nothing; kwargs...)
 
 Creates a new HTML document filled with `content`.
+
+# Example
+
+```jldoctest
+julia> import EzXML: prettyprint
+
+julia> prettyprint(html())
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<!DOCTYPE html SYSTEM "about:legacy-compat">
+<html/>
+```
 """
 function html(content...=nothing; kwargs...)
     doc = EzXML.HTMLDocumentNode("about:legacy-compat", nothing)
